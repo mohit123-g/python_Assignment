155c1b8780bf3443ce82a96bd73646c8f70570b4
mohit123-g <137385621+mohit123-g@users.noreply.github.com>
2023-06-26 22:55:44 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,74 @@
+print("arithmetic operator")
+n1=20
+n2=5
+print("num1 is:",n1,",num2 is:",n2)
+print()
+print("addition:n1+n2:",n1+n2)
+print("subtraction:n1-n2:",n1-n2)
+print("multiplication:n1*n2:",n1*n2)
+print("division:n1/n2:",n1/n2)
+print("mod:n1%n2:",n1%n2)
+print("floor division:n1//n2:",n1//n2)
+print("exponent:n1**n2:",n1**n2)
+
+print("Assignment operator")
+print()
+print("num1 is:",n1,",num2 is:",n2)
+print("n1=",n1)
+n1+=2
+print("n1+=2:",n1)
+n1-=2
+print("n1-=2:",n1)
+n1*=2
+print("n1*=2:",n1)
+n1/=2
+print("n1/=2:",n1)
+n2%=2
+print("n2%=2:",n1)
+n1**=2
+print("n1**=2:",n1)
+n1//=2
+print("n1//=2:",n1)
+print()
+#Comparison operator
+print()
+print("num1 is:",n1,",num2 is:",n2)
+print("n1>n2:",n1>n2)
+print("n1<n2:",n1<n2)
+print("n1>=n2:",n1>=n2)
+print("n1<=n2:",n1<=n2)
+print("n1==n2:",n1==n2)
+print("n1!=n2:",n1!=n2)
+print()
+print("Logical operator")
+print()
+a=True
+b=False
+print("a is:",a,",b is:",b)
+print("a and b:",a and b)
+print("a or b:",a or b)
+print("not b:",not b)
+print()
+print("Betwise operator")
+print()
+n3=10
+n4=5
+print("num3 is:",n3,",num4 is:",n4)
+print("Bitwise (and)&:n3&n4:",n3&n4)
+print("Bitwise  (or)|:n3|n4:",n3|n4)
+print("Bitwise (xor)^:n3^n4:",n3^n4)
+print("Bitwise (right shift)>>:n3>>n4:",n3>>n4)
+print("Bitwise (left shift)<<:n3<<n4:",n3<<n4)  
+print()
+print("Indentity operator")
+print()
+print("num3 is:",n3,",num4 is:",n4)
+print("n3 is 10:",n3 is 5)
+print("n3 is not 10:",n3 is not 10)
+print()
+print("membership operator")
+print()
+name="Mohit"
+print("name is:",name)
+print(" M in name:","M" in name)
+print("o not in name:","o" not in name)
---
+++ Assignment3.py
@@ -0,0 +1,23 @@
+choice=True
+print("Calculator")
+while choice==True:
+     n1=int(input("Enter first num:"))
+     op=input("Enetr operator(+,-,*,/,%):")
+     n2=int(input("Enter second num:"))
+     if op=="+":
+         print("Ans:",n1,"+",n2,":",n1+n2)
+     elif op=="-":
+         print("Ans:",n1,"-",n2,":",n1-n2)
+     elif op=="*":
+         print("Ans:",n1,"*",n2,":",n1*n2)
+     elif op=="/":
+         print("Ans:",n1,"/",n2,":",n1/n2)
+     elif op=="%": 
+         print("Ans:",n1,"%",n2,":",n1%n2)
+     else:
+         print("invalid operator entered")
+
+     print("Enter 1 to continue or 0 for exit:")
+     cho=int(input())
+     if cho==0:  
+         choice=False
