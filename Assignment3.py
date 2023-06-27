03835311e59f181994b8e964d631fc6b4f4d9a76
mohit123-g <137385621+mohit123-g@users.noreply.github.com>
2023-06-27 13:20:00 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,23 +1,23 @@
 choice=True
 print("Calculator")
 while choice==True:
-     n1=int(input("Enter first num:"))
+     n1=input("Enter first num:")
      op=input("Enetr operator like(+,-,*,/,%,**,//):")
-     n2=int(input("Enter second num:"))
+     n2=input("Enter second num:")
      if op=="+":
-         print("Ans:",n1,"+",n2,":",n1+n2)
+         print("Ans:",n1,"+",n2,":",(float(n1)+float(n2)))
      elif op=="-":
-         print("Ans:",n1,"-",n2,":",n1-n2)
+         print("Ans:",n1,"-",n2,":",(float(n1)-float(n2)))
      elif op=="*":
-         print("Ans:",n1,"*",n2,":",n1*n2)
+         print("Ans:",n1,"*",n2,":",(float(n1)*float(n2)))
      elif op=="/":
-         print("Ans:",n1,"/",n2,":",n1/n2)
+         print("Ans:",n1,"/",n2,":",(float(n1)/float(n2)))
      elif op=="%": 
-         print("Ans:",n1,"%",n2,":",n1%n2)
+         print("Ans:",n1,"%",n2,":",(float(n1)%float(n2)))
      elif op=="**":
-         print("Ans:",n1,"**",n2,":",n1**n2)
+         print("Ans:",n1,"**",n2,":",(float(n1)**float(n2)))
      elif op=="//": 
-         print("Ans:",n1,"//",n2,":",n1//n2)
+         print("Ans:",n1,"//",n2,":",(float(n1)//float(n2)))
      else:
          print("invalid operator entered")
 
